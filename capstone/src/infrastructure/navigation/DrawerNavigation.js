11bae9e1a960853710d1bfb387c9d479259e5aa6
emmasmth <90426359+emmasmth@users.noreply.github.com>
2024-04-04 22:27:42 -0400

    home,login,registration nav

--- capstone/src/infrastructure/navigation/DrawerNavigation.js
+++ capstone/src/infrastructure/navigation/DrawerNavigation.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import {Ionicons, SimpleLineIcons, AntDesign} from "@expo/vector-icons";
+
+import HomeScreen from '../../features/account/screens/HomeScreen';
 import LoginScreen from '../../features/account/screens/LoginScreen';
 import RegistrationScreen from '../../features/account/screens/RegistrationScreen';
 
@@ -9,9 +12,27 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigation = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator initialRouteName='LoginScreen'>
-                <Drawer.Screen name='LoginScreen' component={LoginScreen}/>
-                <Drawer.Screen name='RegistrationScreen' component={RegistrationScreen}/>
+            <Drawer.Navigator initialRouteName='HomeScreen'>
+                <Drawer.Screen 
+                    name="Home Screen" 
+                    component={HomeScreen} 
+                    options={{drawerIcon: ({focused, size}) => (
+                        <Ionicons name = 'home' size = {size} color = {focused} /> )}}
+                />
+
+                <Drawer.Screen 
+                    name='Login Screen' 
+                    component={LoginScreen}
+                    options={{drawerIcon: ({focused, size}) => (
+                        <SimpleLineIcons name = 'login' size = {size} color = {focused} /> )}}
+                />
+
+                <Drawer.Screen 
+                    name='Registration Screen'
+                    component={RegistrationScreen}
+                    options={{drawerIcon: ({focused, size}) => (
+                        <AntDesign name = 'login' size = {size} color = {focused} /> )}}
+                />
             </Drawer.Navigator>
         </NavigationContainer>
     );
