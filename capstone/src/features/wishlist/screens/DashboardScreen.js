3163568525a7bb7f06d2edc84d391d8db7d91a77
emmasmth <90426359+emmasmth@users.noreply.github.com>
2024-04-18 23:34:42 -0400

    one wishlist view

--- capstone/src/features/wishlist/screens/DashboardScreen.js
+++ capstone/src/features/wishlist/screens/DashboardScreen.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import { ScrollView, View, Text, Button, Alert } from 'react-native';
 import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { getFirestore, doc, getDocs, collection, collections, listCollections, QuerySnapshot } from 'firebase/firestore';
 
 const DashboardScreen = ({ navigation }) => {
     const [user, setUser] = useState(null);
-    const [userData, setUserData] = useState(true);
-    const [userData2, setUserData2] = useState(true);
-    const [otherCollections, setOtherCollections] = useState([]);
+    const [justBecause, setJustBecause] = useState([]);
   
     useEffect(() => {
       const auth = getAuth();
@@ -17,16 +15,15 @@ const DashboardScreen = ({ navigation }) => {
         {
           const db = getFirestore();
           const userRef = doc(db, 'users', user.uid);
-          const userInfoRef = collection(userRef, 'test');
+          const docRef = collection(userRef, 'Just Because');
           
-          getDocs(userInfoRef)
+          getDocs(docRef)
             .then((querySnapshot) => {
-                if(!querySnapshot.empty) {
-                    const userData = querySnapshot.docs[0].data();
-                    setUserData(userData);
-                    const userData2 = querySnapshot.docs[1].data();
-                    setUserData2(userData2);
-                  }
+                const userDataArray = [];
+                querySnapshot.forEach((doc) => {
+                  userDataArray.push(doc.data());
+                });
+                setJustBecause(userDataArray);
             })
             .catch((error) => {
                 console.error('Error fetching other collections: ', error);
@@ -39,17 +36,33 @@ const DashboardScreen = ({ navigation }) => {
     }, []);
   
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'left', marginLeft: 30 }}>
         {user ? (
-          <>
-            <Text>List: {userData.list}</Text>
-            <Text>item name 1: {userData.item}</Text>
-            <Text>item name 2: {userData2.item}</Text>
-          </>
+            justBecause.map((userData, index) => (
+              <View key = {index}>
+                {index === 0 || userData.list !== justBecause[index - 1].list ? (
+                  <Text style = {{fontSize: 40}}>List: {userData.list}</Text>
+                ) : null}
+                <Text>Item: {userData.item}</Text>
+                <Text>Price: {userData.price}</Text>
+                <Text>Tax: {userData.tax}</Text>
+                <Text>Tip: {userData.tip}</Text>
+                <Text>Store Name: {userData.store}</Text>
+                <Text>Store URL: {userData.url}</Text>
+                <Text>Store Shoutout: {userData.shoutout}</Text>
+                <Text>Event: {userData.event}</Text>
+                <Text>Event Date: {userData.eventDate}</Text>
+                <Text>Item Description: {userData.description}</Text>
+                <Text>Type: {userData.type}</Text>
+                {index < justBecause.length - 1 && <View style={{ marginBottom: 20 }} />}
+              </View>
+            ))
         ) : (
           <Text>Please log in or register to view your wishlists.</Text>
         )}
       </View>
+      </ScrollView>
     );
   };
 
